5b184550c78a000376e0e9606d3649406f939fad
Junichi Kato <j5ik2o@gmail.com>
2016-12-17 21:37:24 +0900

    refactor

--- setup.sh
+++ setup.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
+CURRENT_DIR=$(pwd)
+
 DOT_FILES=(.zsh .zshrc .zshrc.alias .zshrc.linux .zshrc.osx .gitconfig .gitignore .sbtconfig .vimrc .vim .tmux.conf .dircolors)
 
 for file in ${DOT_FILES[@]}; do 
-    ln -s $HOME/dotfiles/$file $HOME/$file
+    ln -s ${CURRENT_DIR}/$file $HOME/$file
 done
-
-[ ! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-
-[ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && vim -c ':NeoBundleInstall'
-
-[ ! -d ~/.nvm ] && git clone git://github.com/creationix/nvm.git ~/.nvm
 
 case "${OSTYPE}" in
 darwin*)
     brew update
-    brew tap josegonzalez/php
     brew install scala rbenv ruby-build zsh tmux wget curl coreutils reattach-to-user-namespace
     ;;
 linux*)
