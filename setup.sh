87fb8cd1adc6c49e35540aceea9ad08ba16beea2
Junichi Kato <j5ik2o@gmail.com>
2018-06-11 17:56:44 +0900

    refactor

--- setup.sh
+++ setup.sh
@@ -2,11 +2,6 @@
 
 CURRENT_DIR=$(cd $(dirname $0) && pwd)
 
-mkdir -p ~/.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-
-curl -L git.io/nodebrew | perl - setup
-
 DOT_FILES=(.zsh .zshrc .zshrc.alias .zshrc.linux .zshrc.osx .gitconfig .gitignore .sbtconfig .vimrc .vimrc.basic .vimrc.bundle .vimrc.colors .vimrc.encoding .vimrc.plugin_setting .vim .tmux.conf .dircolors)
 
 for file in ${DOT_FILES[@]}; do 
@@ -21,3 +16,8 @@ darwin*)
 linux*)
     ;;
 esac
+
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+
+curl -L git.io/nodebrew | perl - setup
+
