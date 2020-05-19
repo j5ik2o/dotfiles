d258e7484231ef716cd2599a6e6a50990340bbf4
Junichi Kato <j5ik2o@gmail.com>
2020-05-19 20:58:52 +0900

    update

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ CURRENT_DIR=$(cd $(dirname $0) && pwd)
 
 DOT_FILES=(.p10k.zsh .zsh .zshrc .zshrc.alias .zshrc.common .zshrc.linux .zshrc.osx .gitconfig .gitignore .sbtconfig .vimrc .vimrc.basic .vimrc.bundle .vimrc.colors .vimrc.encoding .vimrc.plugin_setting .vimrcs.plugin.toml .vimrc.plugins_lazy.toml .vim .tmux.conf .dircolors)
 
-for file in ${DOT_FILES[@]}; do 
-    ln -s ${CURRENT_DIR}/$file $HOME/$file
+for FILE in ${DOT_FILES[@]}; do 
+    [ ! -f $HOME/$file ] && ln -s ${CURRENT_DIR}/$file ${HOME}/$FILE && echo "link ${CURRENT_DIR}/${FILE} -> ${HOME}/${FILE}"
 done
