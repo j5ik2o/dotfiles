c4b6b2563bc35849b8057f614912d2a26d4a6436
Junichi Kato <j5ik2o@gmail.com>
2026-03-15 09:46:18 +0900

    chore(homebrew): add verbose flag to brew options

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -13,6 +13,7 @@
       # "none" = 何もしない, "uninstall" = formulae のみ削除, "zap" = 全削除
       cleanup = lib.mkDefault "none";
       upgrade = true;
+      extraFlags = [ "--verbose" ];
     };
 
     # Homebrew taps
